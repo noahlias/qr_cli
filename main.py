6952e852280d30372e0be795e0acf1c667466226
Alias <yygggg@foxmail.com>
2023-05-23 16:09:03 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import click
+import cv2
+from pyzbar import pyzbar
+import qrcode
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("image", type=click.Path(exists=True))
+def scan(image):
+    """Scan QR codes in an image"""
+    image = cv2.imread(image)
+    qr_codes = pyzbar.decode(image)
+
+    if qr_codes:
+        for qr_code in qr_codes:
+            data = qr_code.data.decode("utf-8")
+            click.echo(f"QR Code Data: {data}")
+    else:
+        click.echo("No QR codes found in the image")
+
+
+@cli.command()
+@click.argument("data")
+@click.argument("output", type=click.Path())
+def generate(data, output):
+    """Generate a QR code"""
+    qr = qrcode.QRCode()
+    qr.add_data(data)
+    qr.make()
+    img = qr.make_image(fill="black", back_color="white")
+    img.save(output)
+
+
+if __name__ == "__main__":
+    cli()
